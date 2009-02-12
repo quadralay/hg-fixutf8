4270f2181dd07adcd86c1acbcf527c3b1139c958
Stefan Rusek <stefan@rusek.org>
2009-02-12 22:00:10 +0100

    Use ctypes to call the proper WIN32 apis

--- fixutf8.py
+++ fixutf8.py
@@ -18,11 +18,121 @@ Fix incompatibilities with non-utf8 locales
 No special configuration is needed.
 '''
 
+#
+# How it works:
+#
+#  There are 2 ways for strings to get into HG, either
+# via that command line or filesystem filename. We want
+# to make sure that both of those work.
+#
+#  We use the WIN32 GetCommandLineW() to get the unicode
+# version of the command line. And we wrapp all the
+# places where we send or get filenames from the os and
+# make sure we send UCS-16 to windows and convert back
+# to UTF8.
+#
+#  There are bugs in Python that make print() and
+# sys.stdout.write() barf on unicode or utf8 when the
+# output codepage is set to 65001 (UTF8). So we do all
+# outputing via WriteFile() with the code page set to
+# 65001. The trick is to save the existing codepage,
+# and restore it before we return back to python.
+#
+#  The result is that all of our strings are UTF8 all
+# the time, and never explicitly converted to anything
+# else.
+#
+
 import sys, os, shutil
+
+stdout = sys.stdout
+
 from mercurial import util, osutil, dispatch, extensions, i18n
 import stat as _stat
 import mercurial.ui as _ui
 
+try:
+    from ctypes import *
+except:
+    pass
+
+def test():
+    print win32getargs()
+    print sys.argv
+
+    uargs = ['P:\\hg-fixutf8\\fixutf8.py', 'thi\xc5\x9b', 'i\xc5\x9b',
+            '\xc4\x85', 't\xc4\x99\xc5\x9bt']
+    for s in uargs:
+        rawprint(hStdOut, s + "\n")
+
+# Using ctypes we can call the unicode versions of win32 api calls that 
+# python does not call.
+if sys.platform == "win32" and windll:
+    LPCWSTR = c_wchar_p
+    LPCSTR = c_char_p
+    INT = c_int
+    UINT = c_uint
+    BOOL = INT
+    DWORD = UINT
+    HANDLE = c_void_p
+
+    prototype = WINFUNCTYPE(LPCWSTR)
+    GetCommandLine = prototype(("GetCommandLineW", windll.kernel32))
+
+    prototype = WINFUNCTYPE(POINTER(LPCWSTR), LPCWSTR, POINTER(INT))
+    CommandLineToArgv = prototype(("CommandLineToArgvW", windll.shell32))
+
+    prototype = WINFUNCTYPE(BOOL, UINT)
+    SetConsoleOutputCP = prototype(("SetConsoleOutputCP", windll.kernel32))
+
+    prototype = WINFUNCTYPE(UINT)
+    GetConsoleOutputCP = prototype(("GetConsoleOutputCP", windll.kernel32))
+
+    prototype = WINFUNCTYPE(DWORD)
+    GetLastError = prototype(("GetLastError", windll.kernel32))
+
+    prototype = WINFUNCTYPE(HANDLE, DWORD)
+    GetStdHandle = prototype(("GetStdHandle", windll.kernel32))
+
+    prototype = WINFUNCTYPE(BOOL, HANDLE, LPCSTR, DWORD,
+            POINTER(DWORD), DWORD)
+    WriteFile = prototype(("WriteFile", windll.kernel32))
+
+    hStdOut = GetStdHandle(0xFFFFfff5)
+    hStdErr = GetStdHandle(0xFFFFfff4)
+
+    def rawprint(h, s):
+        try:
+            oldcp = GetConsoleOutputCP()
+            SetConsoleOutputCP(65001)
+            limit = 0x4000
+            l = len(s)
+            start = 0
+            while start < l:
+                end = start + limit
+                buffer = s[start:end]
+                c = DWORD(0)
+                if not WriteFile(h, buffer, len(buffer), byref(c), 0):
+                    raise pywintypes.error(err, "WriteFile",
+                            win32api.FormatMessage(err))
+                start = start + c.value + 1
+        finally:
+            SetConsoleOutputCP(oldcp)
+
+    def win32getargs():
+        '''
+        win32getargs() -> [args]
+
+        Returns an array of utf8 encoded arguments passed on the command line.
+        '''
+        c = INT(0)
+        pargv = CommandLineToArgv(GetCommandLine(), byref(c))
+        return [fromunicode(pargv[i]) for i in xrange(1, c.value)]
+else:
+    rawprint = False
+    win32getargs = False
+    hStdOut = 0
+    hStdErr = 0
 
 def mapconvert(convert, canconvert, doc):
     '''
@@ -57,7 +167,7 @@ tounicode = mapconvert(
     "Convert a UTF-8 byte string to Unicode")
 
 fromunicode = mapconvert(
-    lambda s: s.encode('utf-8'),
+    lambda s: makesafe(s.encode('utf-8')),
     lambda s: isinstance(s, unicode),
     "Convert a Unicode string to a UTF-8 byte string")
 
@@ -69,6 +179,10 @@ def safefromlocal(orig, s):
     r = orig(s)
     fromlocalresults.append(r)
     return r
+
+def makesafe(s):
+    fromlocalresults.append(s)
+    return s
 
 oldtolocal = util.tolocal
 def _tolocal(s):
@@ -126,26 +240,31 @@ def listdir(path, stat=False, skip=None):
     return result
 
 def uisetup(ui):
+    if sys.platform != 'win32':
+        return
+
     convert =  mapconvert(
         lambda s: oldtolocal(s),
         lambda s: isinstance(s, str),
         "Converts to local codepage")
-    def localize(orig, ui, *args):
-        if ui.isatty() and not ui.buffers:
-            orig(ui, *convert(args))
-        else:
-            orig(ui, *args)
+    def localize(h):
+        def f(orig, ui, *args):
+            if not ui.buffers:
+                if rawprint:
+                    rawprint(h, ''.join(args))
+                else:
+                    orig(ui, *convert(args))
+            else:
+                orig(ui, *args)
+        return f
 
-    extensions.wrapfunction(_ui.ui, "write", localize)
-    extensions.wrapfunction(_ui.ui, "write_err", localize)
+    extensions.wrapfunction(_ui.ui, "write", localize(hStdOut))
+    extensions.wrapfunction(_ui.ui, "write_err", localize(hStdErr))
     extensions.wrapfunction(i18n, "gettext", gettextwrapper)
     extensions.wrapfunction(i18n, "_", gettextwrapper)
 
 def extsetup():
-    if util._encoding.lower() == "utf-8":
-        # this will never happen on windows and allows
-        # us to not fix non-windows where the default 
-        # locale is already UTF-8
+    if sys.platform != 'win32':
         return
 
     oldlistdir = osutil.listdir
@@ -155,13 +274,16 @@ def extsetup():
     extensions.wrapfunction(util, "fromlocal", safefromlocal)
     util.tolocal = _tolocal
     
-    extensions.wrapfunction(dispatch, "_parse",
-            lambda orig, ui, args: orig(ui, map(util.fromlocal, args)))
+    if win32getargs:
+        extensions.wrapfunction(dispatch, "_parse",
+                lambda orig, ui, args: orig(ui, win32getargs()[-len(args):]))
+    else:
+        extensions.wrapfunction(dispatch, "_parse",
+                lambda orig, ui, args: orig(ui, map(util.fromlocal, args)))
 
-    posixfile__init__ = util.posixfile.__init__
-    class posixfile_utf8(util.posixfile):
+    class posixfile_utf8(file):
         def __init__(self, name, mode='rb'):
-            posixfile__init__(self, tounicode(name), mode)
+            super(posixfile_utf8, self).__init__(tounicode(name), mode)
     util.posixfile = posixfile_utf8
 
     if util.atomictempfile:
@@ -205,3 +327,6 @@ def extsetup():
             'mkdir', 'rename', 'removedirs')
     wrapnames(shutil, 'copyfile', 'copymode')
 
+
+if __name__ == "__main__":
+    test()
